ce7f9f6a3a67cc343d9c49b8a977ae66a46ada23
fireworld <fuxudong103@gmail.com>
2026-02-22 10:00:25 +0800

    尝试自己编译

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.yshs.sync_clipboard_flutter"
+        applicationId = "com.fireworld.sync_clipboard_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
